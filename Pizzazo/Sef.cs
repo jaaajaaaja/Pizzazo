540e071974885f6a73aaa30b103ddd3645f57f74
jaaajaaaja <andkob0126@gmail.com>
2026-02-17 13:12:13 +0100

    sef

---
+++ Pizzazo/Sef.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzazo
+{
+    internal class Sef
+    {
+        private readonly PizzaKemence kemence;
+        private readonly Random random = new Random();
+        private readonly string[] pizzak = { "Margherita", "Sonkás", "Hawaii", "Pepperoni" };
+
+        public Sef(PizzaKemence kemence)
+        {
+            this.kemence = kemence;
+        }
+
+        public void Keszit()
+        {
+            while (true)
+            {
+                string nev = pizzak[random.Next(pizzak.Length)];
+                int meret = random.Next(20, 41);
+                Pizza pizza = new Pizza(nev, meret);
+                kemence.FeladPizza(pizza);
+                Console.WriteLine($"Séf készítette: {pizza}");
+                Thread.Sleep(random.Next(2000, 3001));
+            }
+        }
+    }
+}
