998509ee4faa3ae5e89ec13f19c979569e941384
jaaajaaaja <andkob0126@gmail.com>
2026-02-17 13:06:48 +0100

    kemence

---
+++ Pizzazo/PizzaKemence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzazo
+{
+    internal class PizzaKemence
+    {
+        private readonly SzinkronPuffer<Pizza> puffer;
+
+        public PizzaKemence(int meret)
+        {
+            puffer = new SzinkronPuffer<Pizza>(meret);
+        }
+
+        public void FeladPizza(Pizza pizza)
+        {
+            puffer.Hozzaad(pizza);
+        }
+
+        public Pizza KiveszPizza()
+        {
+            return puffer.Kivesz();
+        }
+    }
+}
