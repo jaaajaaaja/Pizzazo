2ed46a2afee467760821399a4821830e13b4ea45
jaaajaaaja <andkob0126@gmail.com>
2026-02-17 13:18:08 +0100

    futar, program

---
+++ Pizzazo/Futar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzazo
+{
+    internal class Futar
+    {
+        private readonly PizzaKemence kemence;
+
+        public Futar(PizzaKemence kemence)
+        {
+            this.kemence = kemence;
+        }
+
+        public void Szallit()
+        {
+            while (true)
+            {
+                Pizza pizza = kemence.KiveszPizza();
+                Console.WriteLine($"Futár szállította: {pizza}");
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
--- Pizzazo/Program.cs
+++ Pizzazo/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            
+            PizzaKemence kemence = new PizzaKemence(5);
+
+            Sef sef1 = new Sef(kemence);
+            Sef sef2 = new Sef(kemence);
+            Futar futar = new Futar(kemence);
+
+            Thread sefThread1 = new Thread(sef1.Keszit);
+            Thread sefThread2 = new Thread(sef2.Keszit);
+            Thread futarThread = new Thread(futar.Szallit);
+
+            sefThread1.Start();
+            sefThread2.Start();
+            futarThread.Start();
+
+            sefThread1.Join();
+            sefThread2.Join();
+            futarThread.Join();
         }
     }
 }
