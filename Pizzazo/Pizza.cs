787c53e2ca979c408938a2682095733b94444c68
jaaajaaaja <andkob0126@gmail.com>
2026-02-17 12:57:17 +0100

    Add project files.

---
+++ Pizzazo/Pizza.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pizzazo
+{
+    internal class Pizza
+    {
+        private string nev;
+        private int meret;
+
+        public Pizza(string nev, int meret)
+        {
+            this.nev = nev;
+            this.meret = meret;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.nev} ({this.meret} cm)";
+        }
+    }
+}
